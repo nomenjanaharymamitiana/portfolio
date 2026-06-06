64af8b557afa70671910c21c97589667e009b22c
nomenjanaharymamitiana <tinamamy28@gmail.com>
2026-06-06 06:11:51 +0300

    premier commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbopack: {
+      root: __dirname,
+    },
   },
-  reactStrictMode: false,
 };
 
 export default nextConfig;
